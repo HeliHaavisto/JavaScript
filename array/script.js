e4eac149b7261c5c7641d959585976975d045b46
Haavisto Heli <s2100133@edu.bc.fi>
2021-01-27 18:58:39 +0200

    array task added

---
+++ array/script.js
@@ -0,0 +1,68 @@
+let lista=[];
+
+for ( let i=0 ; i < 5; i++) {
+
+    let luku= Math.floor(Math.random()*6);
+    lista.push(luku);
+    
+}
+
+console.log(lista);
+
+let x=0;
+let counter1=0;
+let counter2=0;
+
+while (x < 5) {
+
+    // Jackpot is now coming with five ones , if wanted to have with five fives, just set next if to 5
+
+    if (lista[x]==1){
+
+        counter1++;
+
+    } else {
+
+        counter2++;
+    }
+    
+    x++;
+    
+}
+
+let roundCounter=0;
+
+while (true) {
+
+    if (counter1==5) {
+
+    console.log("Jackpot!");
+
+    break;
+
+    } else if (counter2==5) {
+
+    console.log("Congratulations!");
+
+    } else if (counter1==4) {
+
+    console.log("smaller price");
+    }
+
+    roundCounter++;
+
+}
+
+let result = Number(roundCounter) *0.5;
+
+console.log( "money spent " + result);
+
+if ( result < 30000) {
+
+    console.log("You profit "+ (30000 - result));
+
+} else {
+
+    console.log ("You lose  " + (result - 30000 ));
+
+}
--- salary/script.js
+++ salary/script.js
@@ -3,24 +3,24 @@ function calculate () {
     let salary = Number(document.getElementById("salary").value);
     let hours = Number(document.getElementById("hours").value);
     
-    let answer =document.getElementById("answer");
+    let answer = document.getElementById("answer");
 
     let x = salary * hours;
 
-    if (hours < 7) {
+    if (hours <= 7) {
 
         answer.textContent= x;
 
-    } else if ((hours >= 7)  && ( hours < 9)) {
+    } else if ( hours <= 9) {
 
-        answer.textContent= x + (9 - hours) * 0,5 * salary;
+        answer.textContent= x + (9 - hours) * 0.5 * salary;
 
     } else {
 
-        answer.textContent= x + (9 - hours) * 0,5 * salary + (hours - 9) * 1 * salary;
+        answer.textContent= x + 2 * 0.5 * salary + (hours - 9) * 1 * salary;
     }
 
-
+// two hours in above to cover hours from 7 to 9
 
 
 
