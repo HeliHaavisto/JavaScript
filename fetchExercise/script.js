b9455ec5730685315253a9389edd301f2cda5ab2
Haavisto Heli <s2100133@edu.bc.fi>
2021-03-04 17:51:51 +0200

    fetchExercise finished

--- fetchExercise/script.js
+++ fetchExercise/script.js
@@ -3,24 +3,33 @@ fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000')
     .then(resp => resp.json())
     .then(json => {
         pokemons = json.results;
-        console.log(pokemons);
+        pokemons.forEach(addPokemon);
     })
     .catch(err => {
-        console.log("error has occurrer" + err);
+        console.log("error has occurred" + err);
     })
 
 
 
-function addPokemon(name) {
-    var table = document.getElementById("pokemonTable");
 
+
+function addPokemon(pokemon) {
+    var table = document.getElementById("pokemonTable");
+    let onePokemon = [];
     var row = table.insertRow();
     var pokemonName = row.insertCell(0);
 
-    pokemonName.innerHTML = "<a href=#>" + name + "</a>";
+    fetch(pokemon.url)
+        .then(resp => resp.json())
+        .then(json => {
+            // console.log(json);
+            onePokemon = json;
+            // console.log(onePokemon);
+
+            pokemonName.innerHTML = "<a href=" + onePokemon.sprites.front_default + " >" + pokemon.name + "</a>";
+        })
+
+
+    // pokemonName.innerHTML = "<a href=" + pokemon.url + ">" + pokemon.name + "</a>";
 }
 
-pokemons.forEach(i => {
-
-    addPokemon([i].name);
-});
